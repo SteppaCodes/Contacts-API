6f0f4973653d2d757ed5fe23dcbacbe576c3781c
Big Steppa <steppacodes@gmail.com>
2024-01-07 00:19:57 +0100

    RegisterVieew

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .serialiers import UserSerializer
+
+class RegisterView(APIView):
+    def post(self,request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
