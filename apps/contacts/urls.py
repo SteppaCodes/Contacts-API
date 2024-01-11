b307985014ca88a3bd1ca21922734b93b2ed1410
Big Steppa <steppacodes@gmail.com>
2024-01-11 03:01:08 +0100

    contact detail view

--- apps/contacts/urls.py
+++ apps/contacts/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from . views import ContactListCreateAPIView
+from . views import ContactListCreateAPIView, ContactDetailView
 
 urlpatterns = [
     path("contact-list/", ContactListCreateAPIView.as_view()),
-    #path("contact-list/", ContactListAPIView.as_view()),
+    path("contact-detail/<pk>", ContactDetailView.as_view()),
 ]
--- apps/contacts/views.py
+++ apps/contacts/views.py
@@ -22,3 +22,27 @@ class ContactListCreateAPIView(APIView):
             serializer.save(owner=request.user)
             return Response(serializer.data, status=201)
     
+class ContactDetailView(APIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = ContactSerializer
+
+    def get(self, request, pk):
+        
+        try:
+            contact = Contact.objects.get(pk=pk)
+            serializer = self.serializer_class(contact)
+            return Response(serializer.data)
+        except:
+            return Response({"error": "Contact not found"}, status=404)
+    
+    def put(self, request, pk):
+        contact = Contact.objects.get(pk=pk)
+        serializer = self.serializer_class(contact, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+
+    def delete(self, request, pk):
+        contact = Contact.objects.get(pk=pk)
+        contact.delete()
+        return Response({"Successsful": "Deleted successfully"})
